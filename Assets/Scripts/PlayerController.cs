fbac55818c0aa757b525edbefa86c91e9982f239
Kristen <kristen.marcinek@rutgers.edu>
2022-01-21 17:02:13 -0500

    disable pickups with ontriggerenter

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -34,4 +34,9 @@ public class PlayerController : MonoBehaviour
         rb.AddForce(movement * speed);
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        other.gameObject.SetActive(false);
+    }
+
 }
